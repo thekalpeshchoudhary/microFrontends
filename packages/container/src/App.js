58e56fae4b47c3d0fdc9cf0748f24cc312de7e02
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2024-04-22 22:31:42 +0530

    Test feature

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,9 @@ import MarketingApp from '../components/MarketingApp'
 export default function App() {
     return <div>
         <h1>Hello Container</h1>
+        <h2>
+            New Subtitle
+        </h2>
         <hr/>
         <MarketingApp />;
     </div> 
