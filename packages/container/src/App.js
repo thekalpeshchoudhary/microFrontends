ae94f13cce06f07a69b3cee8648306723472773b
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2024-04-22 19:00:38 +0530

    spelling fix, text update, default styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from '../components/MarketingApp'
 
 export default function App() {
     return <div>
-        <h1>Hello Container!</h1>
+        <h1>Hello Container</h1>
         <hr/>
         <MarketingApp />;
     </div> 
