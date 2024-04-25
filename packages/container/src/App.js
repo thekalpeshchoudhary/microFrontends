8451fac3f33e3b0778d2d896d2c5cfeac96dec57
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2024-04-25 12:57:01 +0530

    Unqiue Classnames added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default function App() {
-    return <BrowserRouter >
-        <div>
-            <Header />
-            <MarketingApp />;
-        </div>
-    </BrowserRouter>
+    return (<BrowserRouter >
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                <Header />
+                <MarketingApp />;
+            </div>
+        </StylesProvider>
+    </BrowserRouter>)
 }
