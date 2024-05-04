6a3aa0d0339dd71598a0246e61698b8a624a8607
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2024-05-04 19:47:14 +0530

    Sign in functionality implementation, auth workflow added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
     StylesProvider,
@@ -15,15 +15,21 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    console.log(isSignedIn)
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
+                    <Header isSignedIn={isSignedIn} onSignOut={ ()=> setIsSignedIn(false)} />
                     <Suspense fallback={<Progress/>}>
                         <Switch>
-                            <Route path="/auth" component={AuthLazy} />
-                            <Route path="/" component={MarketingLazy} />
+                            <Route path="/auth">
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" >
+                                <MarketingLazy />
+                            </Route>
                         </Switch>
                     </Suspense>
                 </div>
